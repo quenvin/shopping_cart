bbbba4b6373c93fde120b59929463aa48cd7c3ea
quenvin <quenvint@hotmail.com>
2018-03-05 20:19:18 +0800

    Added user sign-up and login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: '123@123.com', password: '123456', name: 'admin', admin: true)
+User.create!(email: 'admin@admin.com', password: '123456', name: 'admin', admin: true)
+User.create!(email: 'user@user.com', password: '123456', name: 'user', admin: false)
