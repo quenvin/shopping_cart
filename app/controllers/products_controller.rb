70120f4190f5f53f556a65936b07df35c3de713d
GLstark <Fredrick.tgl@gmail.com>
2018-03-06 12:53:01 +0800

    Added base UI, Filestack, DotENV

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,25 @@
 class ProductsController < ApplicationController
 
+  before_action :authenticate_admin!, :except => [:show, :index]
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 
 end
