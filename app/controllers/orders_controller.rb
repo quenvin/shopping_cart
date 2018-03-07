bf51f7e97cb4cdb03d09d23e6ad874c3bd4778e5
quenvin <quenvint@hotmail.com>
2018-03-07 09:58:18 +0800

    Cleanup codes in orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,13 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = current_user.orders.all
+    @orders = current_user.orders
     user = current_user
     cart = current_user.orders.last
   end
 
   def edit
-    @products = current_user.orders.last.products.all
+    @products = current_user.orders.last.products
     @uniq_products = current_user.orders.last.products.uniq
   end
 
