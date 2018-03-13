368d03fe1509a817e87ca1a5e61f6d050e17b3e9
GLstark <Fredrick.tgl@gmail.com>
2018-03-13 19:44:51 +0800

    mod seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,3 @@
 
 User.create!(email: 'admin@admin.com', password: '123456', name: 'admin', admin: true)
 User.create!(email: 'user@user.com', password: '123456', name: 'user', admin: false)
-
-Product.create(name: 'Product 1', description: "Description for Product 1", price: 11.50)
-Product.create(name: 'Product 2', description: "Description for Product 2", price: 50.60)
-Product.create(name: 'Product 3', description: "Description for Product 3", price: 150.68)
-
-Order.create(user_id: 2, transaction_num: 'asfaf24sd', status: 'Delivered')
-Order.create(user_id: 2, transaction_num: '42qws3fsf', status: 'In Progress')
-
-Ordersproduct.create(product_id: 1, order_id: 1)
-Ordersproduct.create(product_id: 2, order_id: 1)
-Ordersproduct.create(product_id: 3, order_id: 1)
-Ordersproduct.create(product_id: 3, order_id: 1)
-
-Ordersproduct.create(product_id: 1, order_id: 2)
-Ordersproduct.create(product_id: 1, order_id: 2)
-Ordersproduct.create(product_id: 1, order_id: 2)
-Ordersproduct.create(product_id: 1, order_id: 2)
-Ordersproduct.create(product_id: 2, order_id: 2)
-Ordersproduct.create(product_id: 3, order_id: 2)
-Ordersproduct.create(product_id: 3, order_id: 2)
